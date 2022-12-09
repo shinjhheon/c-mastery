a69d69278f4ea0a03ac5d116fc1d599f228c30c6
shinjhheon <113599642+shinjhheon@users.noreply.github.com>
2022-12-09 14:01:52 +0900

    Add files via upload

--- winform3/propertier/AssemblyInfo.cs
+++ winform3/propertier/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("WindowsFormsApp6")]
+[assembly: AssemblyTitle("WindowsFormsApp5")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WindowsFormsApp6")]
+[assembly: AssemblyProduct("WindowsFormsApp5")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 이 프로젝트가 COM에 노출되는 경우 다음 GUID는 typelib의 ID를 나타냅니다.
-[assembly: Guid("d2eeb594-0f3e-46c4-bba2-dec5f65e7510")]
+[assembly: Guid("6c37004f-3bf3-4b0d-b8e9-32346ccac552")]
 
 // 어셈블리의 버전 정보는 다음 네 가지 값으로 구성됩니다.
 //
