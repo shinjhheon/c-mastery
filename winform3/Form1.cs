b494bc21df835edac268b2c010a81c4a3d4a104f
shinjhheon <113599642+shinjhheon@users.noreply.github.com>
2022-12-09 14:00:22 +0900

    Add files via upload

---
+++ winform3/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sum = (int.Parse(comboBox1.Text)) * (int.Parse(comboBox2.Text));
+            textBox1.Text = ((int.Parse(comboBox1.Text)) * (int.Parse(comboBox2.Text))).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
