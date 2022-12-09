1853427b09ce1acae8a8edaeb401ab4a9ec003fb
shinjhheon <113599642+shinjhheon@users.noreply.github.com>
2022-12-09 13:53:14 +0900

    Add files via upload

---
+++ winform2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Char.IsDigit(e.KeyChar)
+
+                     || e.KeyChar == Convert.ToInt32(Keys.Back)
+
+                     || (e.KeyChar == '-')
+                )
+
+            { 
+
+            }
+
+            else
+
+            {
+                e.Handled = true; 
+
+            }
+        }
+    }
+}
+    
+
